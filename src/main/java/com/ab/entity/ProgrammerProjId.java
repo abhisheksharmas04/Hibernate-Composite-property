2151f162caf414fb2a0cee2c5a3e8f458cc55b5e
abhisheksharmas04 <abhisheksharmaas04@gmail.com>
2023-01-19 06:12:24 +0530

    annotation driven composite id

--- src/main/java/com/ab/entity/ProgrammerProjId.java
+++ src/main/java/com/ab/entity/ProgrammerProjId.java
@@ -2,35 +2,19 @@ package com.ab.entity;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProgrammerProjId implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	//@Type(type="int") -- to tell hibernate to use his predefined dataypes
 	private Integer pid;
 	private Integer projId;
-
-	public ProgrammerProjId() {
-	}
-
-	public ProgrammerProjId(Integer pid, Integer projId) {
-		super();
-		this.pid = pid;
-		this.projId = projId;
-	}
-
-	public Integer getPid() {
-		return pid;
-	}
-
-	public void setPid(Integer pid) {
-		this.pid = pid;
-	}
-
-	public Integer getProjId() {
-		return projId;
-	}
-
-	public void setProjId(Integer projId) {
-		this.projId = projId;
-	}
-
 }
--- src/main/java/com/ab/entity/ProgrammerProjectInfo.java
+++ src/main/java/com/ab/entity/ProgrammerProjectInfo.java
@@ -1,52 +1,25 @@
 package com.ab.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PROGRAMMERS_PROJECT_INFO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProgrammerProjectInfo {
+	@EmbeddedId
 	private ProgrammerProjId id; // Has a property
+	@Column(length = 20)
 	private String pname;
+	@Column(length = 20)
 	private String projName;
 	private Integer teamSize;
-
-	public ProgrammerProjectInfo() {
-	}
-
-	public ProgrammerProjectInfo(ProgrammerProjId id, String pname, String projName, Integer teamSize) {
-		super();
-		this.id = id;
-		this.pname = pname;
-		this.projName = projName;
-		this.teamSize = teamSize;
-	}
-
-	public ProgrammerProjId getId() {
-		return id;
-	}
-
-	public void setId(ProgrammerProjId id) {
-		this.id = id;
-	}
-
-	public String getPname() {
-		return pname;
-	}
-
-	public void setPname(String pname) {
-		this.pname = pname;
-	}
-
-	public String getProjName() {
-		return projName;
-	}
-
-	public void setProjName(String projName) {
-		this.projName = projName;
-	}
-
-	public Integer getTeamSize() {
-		return teamSize;
-	}
-
-	public void setTeamSize(Integer teamSize) {
-		this.teamSize = teamSize;
-	}
 
 }
